c2cf5bba07ff56e330095c1f3e3051c03319e5fc
mojtaba <mojtabazfr5@gmail.com>
2022-11-12 15:05:53 +0330

    Completion Of Html Codes

--- register.php
+++ register.php
@@ -56,12 +56,17 @@ if(isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <input type="text" name="username" value="<?php echo htmlspecialchars($userName); ?>">
+        <div> <?php echo $errors['username']; ?> </div>
+        <input type="password" name="password" value="<?php echo htmlspecialchars($password); ?>">
+        <div> <?php echo $errors['password']; ?> </div>
+        <input type="password" name="password" value="<?php echo htmlspecialchars($passwordRep); ?>">
+        <div> <?php echo $errors['passwordrep']; ?> </div>
+        <input type="submit" name="submit" value="Submit">
+    </form>
 </body>
 </html>
