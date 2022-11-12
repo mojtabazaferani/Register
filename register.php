e707521bd551f725edcdc7eeda0efd1ebb131205
mojtaba <mojtabazfr5@gmail.com>
2022-11-12 14:54:36 +0330

    Start Html Tags

--- register.php
+++ register.php
@@ -44,7 +44,7 @@ if(isset($_POST['submit'])) {
         if($passwordRep !== $password) {
 
             $errors['passwordrep'] = 'Sorry, Password Not Match';
-            
+
         }else {
             // echo '????'
         }
@@ -52,3 +52,16 @@ if(isset($_POST['submit'])) {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
