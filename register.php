92b873a9fd4eff06aaf7f11765925516c811a34e
mojtaba <mojtabazfr5@gmail.com>
2022-11-12 14:52:59 +0330

    End Of register.php File Codes

--- register.php
+++ register.php
@@ -1,7 +1,54 @@
 <?php
 
+$errors = ['username' => '', 'password' => '', 'passwordrep' => ''];
+
 if(isset($_POST['submit'])) {
-    
+
+    if(empty($_POST['username'])) {
+
+        $errors['username'] = 'Empty Username';
+
+    }else {
+
+        $userName = htmlspecialchars($_POST['username']);
+
+        if(! filter_var($userName, FILTER_VALIDATE_EMAIL)) {
+
+            $errors['username'] = 'Email Is Not Valid!!';
+
+        }else {
+
+            echo 'Validate';
+
+        }
+    }
+
+    if(empty($_POST['password'])) {
+
+        $errors['password'] = 'Empty Password';
+
+    }else {
+
+        $password = md5($_POST['password']);
+
+    }
+
+    if(empty($_POST['passwordrep'])) {
+
+        $errors['passwordrep'] = 'Empty Password Rep';
+
+    }else {
+
+        $passwordRep = htmlspecialchars($_POST['passwordrep']);
+
+        if($passwordRep !== $password) {
+
+            $errors['passwordrep'] = 'Sorry, Password Not Match';
+            
+        }else {
+            // echo '????'
+        }
+    }
 }
 
 ?>
